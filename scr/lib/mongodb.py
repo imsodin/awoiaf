ccd49998182975295061469494187d885cc684c9
Guy Yachdav <gyachdav@rostlab.org>
2015-01-05 16:54:53 -0500

    reorged repo

--- scr/lib/mongodb.py
+++ scr/lib/mongodb.py
@@ -1,7 +1,5 @@
 from pymongo import MongoClient
 
-
-
 class MongoDB(object):
   """docstring for MongoDB"""
   def __init__(self, arg):
@@ -10,6 +8,3 @@ class MongoDB(object):
     client = MongoClient('mongodb://localhost:27017/')
     db = client['local']
     self.arg['collection_ref'] = db[arg['collection']]
-
-
-
---
+++ scr/submitJobs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+run_script=$1
+run_params=$2
+
+qsub run_script run_params
