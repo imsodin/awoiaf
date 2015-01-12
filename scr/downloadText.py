651308bc6d18f24d5705fa865f6717a86e598258
Guy Yachdav <gyachdav@rostlab.org>
2015-01-11 23:46:13 -0500

    added text downloader functionality

---
+++ scr/downloadText.py
@@ -0,0 +1,49 @@
+import argparse
+import urllib
+import os
+import io
+import ConfigParser
+import traceback
+from pprint import pprint
+from Charachters import Charachters
+
+class RootDirException(Exception):
+    pass
+
+# Command line options
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+parser.add_argument("-c", "--charachter", type=str, help="charachter name to fetch details")
+args = parser.parse_args()
+
+# Config options
+try:
+    config = ConfigParser.ConfigParser()
+    config.read(os.path.expanduser('~/.awoiafrc'))
+    rootdir = config.get('Folders', 'rootdir')
+    if not rootdir or not os.path.isdir(rootdir):
+        raise RootDirException()
+except Exception, e:
+    traceback.print_exc()
+
+# app begins
+nlp_dir = rootdir+"/Data/text/"
+chars = Charachters(dict())
+
+if args.charachter:
+    char_name = urllib.quote(args.charachter)
+    res = chars.getText(char_name)
+
+    if not os.path.isdir(nlp_dir):
+      os.mkdir(nlp_dir)
+    out_file = nlp_dir+char_name
+    f = io.open(out_file, 'w', encoding='utf8')
+    f.write(unicode(res['text']))
+    f.close()
+
+    # pprint(res)
+
+
+
+
+
