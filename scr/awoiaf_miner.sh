16f8882777b86db9d8c3790f445e89e0c0750f04
Guy Yachdav <gyachdav@gmail.com>
2016-02-07 14:06:20 +0100

    refactoring

--- scr/awoiaf_miner.sh
+++ scr/awoiaf_miner.sh
@@ -3,4 +3,5 @@ set -x
 PYTHONPATH="${PYTHONPATH}:/mnt/home/gyachdav/local/lib/python2.7/site-packages:/mnt/home/gyachdav/Development/awoiaf/scr/lib"
 export PYTHONPATH
 
-/usr/bin/python /mnt/home/gyachdav/Development/awoiaf/scr/mineCharDetails.py  -c $1
+#/usr/bin/python /mnt/home/gyachdav/Development/awoiaf/scr/downloadText.py  -c $1
+/usr/bin/python /mnt/home/gyachdav/Development/awoiaf/scr/mineCharDetails.py -c $1
