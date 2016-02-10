ecb02953563ac3bec3aeb5d50962d3eac30fee95
Guy Yachdav <gyachdav@rostlab.org>
2016-02-10 02:01:20 +0200

    Refactored code to process great houses of westeros data

--- scr/lib/Houses.py
+++ scr/lib/Houses.py
@@ -1,61 +1,124 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from Page import Page
 import urllib
-import io
+# import io
 from pprint import pprint
-from mongodb import MongoDB
-import sys
+# from mongodb import MongoDB
+# import sys
+from xml.dom import minidom
+
+class HousesPage(Page):
+    """docstring for HousesPage
+
+    Attributes:
+        arg (TYPE): Description
+    """
+    def __init__(self, arg):
+        """Summary
+
+        Args:
+            arg (TYPE): Description
+        """
+        super(HousesPage, self).__init__()
+        self.arg = arg
+        # self.arg['houses_list_file'] = '../Data/list_of_IAF_houses'
+        # self.arg['houses_details_file']  = '../Data/houses_details'
+
+    def getListOfHouses(self):
+        """Returns a list of the great houses of westeros
+
+        Returns:
+            list: houses names
+        """
+        offset = 0
+        max_houses = 460
+        list_of_houses  = []
+
+        self.setParams('action', 'query')
+        self.setParams('list', 'search')
+        self.setParams('srsearch', 'house')
+        self.setParams('format', 'xml')
 
 
-class Houses(object):
-  """docstring for Houses"""
-  def __init__(self, arg):
-    super(Houses, self).__init__()
-    self.arg = arg
-    self.arg['houses_list_file'] = '../Data/list_of_IAF_houses'
-    self.arg['houses_details_file']  = '../Data/houses_details'
+        while offset <= max_houses:
+            self.setParams('sroffset', str(offset))
+            self.fetchPage()
+            _request = self.getRequest()
+            _xmldoc = minidom.parseString(_request.content)
+            _item_list = _xmldoc.getElementsByTagName('p')
 
-  def get_house_info(self,house_name):
+            for _item in _item_list:
+                list_of_houses.append(_item.attributes['title'].value)
+            #offset must stay at 10 due to wiki max_links restriction
+            offset += 10
+        return list_of_houses
 
-    awoif_url = 'http://awoiaf.westeros.org/api.php?action=parse&page='+urllib.quote(house_name)+'&format=json&section=0&prop=text'
-    r=requests.get(awoif_url)
-    d = json.loads(r.content)
-    html_to_parse =  d['parse']['text']['*']
-    html_to_parse = html_to_parse.replace('<br>', '\n')
-    parsed_html = BeautifulSoup(html_to_parse)
+    def getHouseInfo(self, house_name):
+        """Returns JSON formatted data structure for house_name
 
-    info = dict()
-    data = []
+        Args:
+            house_name (sttring): name of house (spaces allowed)
 
-    rows = parsed_html.findAll('tr')
-    for row in rows:
-      tKey = tVal = ''
-      cols_head = row.find('th')
-      if cols_head:
-        tKey =  cols_head.get_text()
-      cols_data = row.find('td')
-      if cols_data:
-        tVal =  cols_data.get_text()
-      if tKey and not tVal:
-        info[u'Name']  =  [unicode(house_name)]
-      elif tKey:
-        info[tKey] = tVal.split('\n')
-    return info
+        Returns:
+            dict: JSON formatted data for house. empty if house not found
+        """
+        self.setParams('page', house_name)
+        self.setParams('prop', 'text')
+        self.setParams('section', 0)
+        self.fetchPage()
+        _request = self.getRequest()
+        _data = json.loads(_request.content)
+        _data = _data['parse']['text']['*']
+         # mine info from text
+        _data = self.parseHTML(_data, house_name)
+        return _data
 
-  def get_houses_loyalty(self):
-    cur=mong.arg['collection_ref'].find({},{"Name":1,"Overlord":1})
-    for c in cur:
-      try:
-        s =  c['Name'][0].rstrip().lstrip()
-        o = c['Overlord'][0].rstrip().lstrip()
-        seq = (s,o)
-        print "\t\t\t".join(seq)
-      except KeyError:
-        continue
+    def parseHTML(self, html_to_parse, house_name):
+        """Parses data from info box
 
-mong = MongoDB(dict({'collection':'Houses'}))
-houses = Houses(dict())
+        Args:
+            html_to_parse (string): input text
+
+        Returns:
+            dict: JSON formatted house data
+        """
+        html_to_parse = html_to_parse.replace('<br>', '\n')
+        parsed_html = BeautifulSoup(html_to_parse)
+
+        info = dict()
+
+        rows = parsed_html.findAll('tr')
+        for row in rows:
+            t_key = t_val = ''
+            cols_head = row.find('th')
+            if cols_head:
+                t_key = cols_head.get_text()
+            cols_data = row.find('td')
+            if cols_data:
+                t_val = cols_data.get_text()
+            if t_key and not t_val:
+                info[u'Name'] = [unicode(house_name)]
+            elif t_key:
+                info[t_key] = t_val.split('\n')
+        return info
+
+
+
+#   def get_houses_loyalty(self):
+#       cur=mong.arg['collection_ref'].find({},{"Name":1,"Overlord":1})
+#       for c in cur:
+#         try:
+#           s =  c['Name'][0].rstrip().lstrip()
+#           o = c['Overlord'][0].rstrip().lstrip()
+#           seq = (s,o)
+#           print "\t\t\t".join(seq)
+#         except KeyError:
+#           continue
+
+# mong = MongoDB(dict({'collection':'HousesPage'}))
+# houses = HousesPage(dict())
 
 # with open(houses.arg['houses_list_file']) as f:
 #       houses_list = f.read().splitlines()
@@ -66,7 +129,7 @@ houses = Houses(dict())
 #   mong.arg['collection_ref'].insert(info)
 
 
-houses.get_houses_loyalty()
+# houses.get_houses_loyalty()
 
 
 
---
+++ scr/mineHousesDetails.py
@@ -0,0 +1,61 @@
+"""Summary
+
+Attributes:
+    app (TYPE): Description
+    args (TYPE): Description
+    houses (TYPE): Description
+    list_of_houses (TYPE): Description
+    parser (TYPE): Description
+"""
+import os
+import argparse
+import urllib
+from pprint import pprint
+from Houses import HousesPage
+from Application import Application
+
+
+class RootDirException(Exception):
+    pass
+
+# Command line options
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+parser.add_argument("-s", "--house", type=str, help="house name to fetch details")
+parser.add_argument("-l", "--list_of_houses", type=str, help="house name to fetch details")
+
+args = parser.parse_args()
+app = Application()
+list_of_houses = []
+
+# app begins
+houses = HousesPage(dict())
+if args.list_of_houses:
+  list_of_houses = houses.getListOfHouses()
+  pprint (list_of_houses)
+
+
+
+
+if args.house:
+    house_name = urllib.quote(args.house)
+
+    file_name_text = '/'.join([app.settings['storage_folder']['Houses_text'],house_name])
+    file_name_info = '/'.join([app.settings['storage_folder']['Houses_info'],house_name])
+    file_name_nlp = '/'.join([app.settings['storage_folder']['Houses_nlp'],house_name])
+
+
+if not os.path.exists(file_name_info):
+  # content = chars.fetchCharachterText(char_name)
+  # app.storeFile(file_name_text, content)
+    content = houses.getHouseInfo(house_name)
+    app.storeFile(file_name_info, content)
+
+# else:
+#   with open(file_name_text, 'r') as myfile:
+#     data=myfile.read().replace('\n', '')
+# content = chars.nlpCharachterInfo(data)
+# app.storeFile(file_name_nlp, content)
+
+
+
