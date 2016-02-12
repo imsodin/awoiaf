a3a5f70fa609dbdbd57ebe48e6c89e45de680d66
Guy Yachdav <gyachdav@gmail.com>
2016-02-12 01:25:14 +0100

    enabled parallel processoing of house infromation fetch and extraction jobs

---
+++ scr/sge/awoiaf_houses_miner.sh
@@ -0,0 +1,8 @@
+set -x
+
+AWOIAF_ROOT=/mnt/home/gyachdav/Development/awoiaf
+
+PYTHONPATH="${PYTHONPATH}:/mnt/home/gyachdav/local/lib/python2.7/site-packages:${AWOIAF_ROOT}/scr/lib"
+export PYTHONPATH
+
+/usr/bin/python ${AWOIAF_ROOT}/scr/mineHousesDetails.py  -s $1
---
+++ scr/sge/submit_jobs_houses.py
@@ -0,0 +1,36 @@
+"""creates SGE jobs to fetch details for each charachters"""
+import argparse
+from subprocess import call
+import os
+import ConfigParser
+import traceback
+from pprint import pprint
+from Houses import HousesPage
+
+
+# Command line options
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+parser.add_argument("-n", "--dryrun", help="don't run system calls", action="store_true")
+args = parser.parse_args()
+
+# Config options
+try:
+  config = ConfigParser.ConfigParser()
+  config.read(os.path.expanduser('~/.awoiafrc'))
+  rootdir = config.get('Folders', 'rootdir')
+  if not rootdir or not os.path.isdir(rootdir):
+    raise RootDirException()
+except Exception, e:
+  traceback.print_exc()
+
+
+_houses = HousesPage(dict())
+_list_of_houses = _houses.getListOfHouses()
+for l in _list_of_houses:
+    l = l.replace(" ", "_").replace("'", "").strip().rstrip().lstrip()
+    _cmd = " ".join(["/usr/bin/qsub", "-j", "y", "-o", "/dev/null", rootdir+"/scr/sge/awoiaf_houses_miner.sh", l])
+    if args.verbose:
+        print _cmd
+    if not args.dryrun:
+        call(["/usr/bin/qsub", "-j", "y", "-o", "/dev/null", rootdir+"/scr/sge/awoiaf_houses_miner.sh", l])
